4a4f40af2bec8f96ee9bbeeef095d3f911f6fa7a
Hinge03 <hingenikhil316@gmail.com>
2025-06-28 13:10:47 +0530

    Third commit

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def clean_text(text):
     text = text.lower()
     text = re.sub(r'[^\w\s]', '', text) 
     words = text.split()
-    words = [lemmatizer.lemmatize(word) for word in words if word not in stop_words]
+    words = [lemmatizer.lemmatize(word,pos='v') for word in words if word not in stop_words]
     return ' '.join(words)
 
 # Recruiter route
